a906c402f587e373fbcedaa06197ddc54a9491fc
Artur Figueiredo <arturfig.br@gmail.com>
2025-12-03 20:07:41 -0300

    Hotfix

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useAuth } from '@/hooks/useAuth';
 
 type GenderOption = 'MALE' | 'FEMALE' | 'OTHER';
